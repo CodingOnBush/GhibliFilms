f39246308ca53ef03895eae6b2138a84c61713c1
CodingOnBush <mostafaomrane4@gmail.com>
2020-05-19 15:48:30 +0200

    App operational with welcome activity

---
+++ app/src/main/java/com/example/ghiblifilms/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.ghiblifilms;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.welcome_activity);
+    }
+}
