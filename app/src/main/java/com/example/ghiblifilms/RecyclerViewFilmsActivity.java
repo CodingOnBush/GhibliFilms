c42a885babdebe2b4b91b22c679a66c6f1f65949
CodingOnBush <mostafaomrane4@gmail.com>
2020-05-19 19:24:55 +0200

    Add constants, internet permissions and complete api call

--- app/src/main/java/com/example/ghiblifilms/RecyclerViewFilmsActivity.java
+++ app/src/main/java/com/example/ghiblifilms/RecyclerViewFilmsActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 
 public class RecyclerViewFilmsActivity extends Activity {
@@ -13,11 +17,23 @@ public class RecyclerViewFilmsActivity extends Activity {
         //TODO add checkbox
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recyclerview_films);
+
+        RecyclerViewFilmsController controller = new RecyclerViewFilmsController(this);
+        controller.onStart();
     }
 
     @Override
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public void showFilmList(ArrayList<Film> filmArrayList){
+        RecyclerView mRecyclerView = this.findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(
+                this,
+                LinearLayoutManager.HORIZONTAL,
+                false);
+        //TODO créer l'adapter
     }
 }
--- app/src/main/java/com/example/ghiblifilms/RecyclerViewFilmsController.java
+++ app/src/main/java/com/example/ghiblifilms/RecyclerViewFilmsController.java
@@ -1,4 +1,57 @@
 package com.example.ghiblifilms;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RecyclerViewFilmsController {
+    private RecyclerViewFilmsActivity view;
+    private ArrayList<Film> filmArrayList;
+
+    public RecyclerViewFilmsController(RecyclerViewFilmsActivity view) {
+        this.view = view;
+    }
+
+    public void onStart(){
+        makeApiCall();
+        view.showFilmList(filmArrayList);
+    }
+
+    public void makeApiCall(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        GhibliApi ghibliApi = retrofit.create(GhibliApi.class);
+
+        Call<RestGhibliFilmsResponse> call = ghibliApi.getRestGhibliFilmsResponse();
+        call.enqueue(new Callback<RestGhibliFilmsResponse>() {
+            @Override
+            public void onResponse(Call<RestGhibliFilmsResponse> call, Response<RestGhibliFilmsResponse> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    filmArrayList = response.body().films;
+                    System.out.println("Api call SUCCESS");
+                }else{
+                    System.out.println("response is not successful or response.body is null");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestGhibliFilmsResponse> call, Throwable t) {
+                System.out.println("Api call failed");
+            }
+        });
+    }
 }
